a8840d07d8fb4e94ada9439bef44b05fa24c6a6a
Natalia Pankowska <npankowska@gmail.com>
2024-09-02 12:48:37 +0200

    display critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
 
-import { Card, CardHeader, CardBody, CardFooter, Image, Heading, Text } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter, Image, Heading, Text, HStack } from '@chakra-ui/react'
 import { Game } from '../assets/hooks/useGame'
 import PlatformIconList from './PlatformIconList'
+import CriticScore from './CriticScore'
 
 interface Props {
  game: Game,
@@ -17,9 +18,10 @@ const GameCard = ({game}: Props) => {
       
         
           <Heading fontSize='xl'>{game.name}</Heading>
-       
+        <HStack justifyContent={'space-between'}>
           <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
-        
+          <CriticScore score={game.metacritic} />
+          </HStack>
     </CardBody>
    </Card>
    </>
