c7786cdd793ed09737a019775520d9472bbc6cc1
Natalia Pankowska <npankowska@gmail.com>
2024-09-02 14:28:43 +0200

    show loading skeletons

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,17 @@
+import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
+
+
+const GameCardSkeleton = () => {
+  return (
+    <>
+    <Card width='300px'> 
+        <Skeleton height='00px' borderRadius={10}/>
+        <CardBody>
+    <SkeletonText />
+        </CardBody>
+    </Card>
+    </>
+  )
+}
+
+export default GameCardSkeleton
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,18 +2,22 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGame from '../assets/hooks/useGame';
 import GameCard from './GameCard';
+import GameCardSkeleton from './GameCardSkeleton';
 
 
 
 const GameGrid = () => {
 
-const {games, error} = useGame()
+const {games, error, isLoading} = useGame();
+const skeletons = [1,2,3,4,5,6];
 // console.log(games[9].image_background)
   return (
     <>
     {error && <Text > {error}</Text>}
     <SimpleGrid columns={{sm: 1, md: 2, lg:3, xl: 4}} spacing={10}>
+    {isLoading && skeletons.map(skel => <GameCardSkeleton key={skel} />)}
     {games.map((game) => <GameCard key= {game.id} game={game}/>)}
+    
     </SimpleGrid>
     </>
   )
